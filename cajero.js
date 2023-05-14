9ce4c4d6d3bd6fba0e4b804088db378a7966ffd1
@RaulHerrera09 <raulherreradelgadillo09@gmail.com>
2023-05-14 10:34:28 -0600

    Variables js

---
+++ cajero.js
@@ -0,0 +1,8 @@
+var accounts = [
+    { name: "Jak", balance: 200, password: "1234" },
+    { name: "Miguel", balance: 290, password: "5678" },
+    { name: "Kevin", balance: 67, password: "9012" },
+  ];
+
+var currentAccount;
+var currentBalance; 
